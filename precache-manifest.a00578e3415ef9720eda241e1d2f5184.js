749274299ae4d1cccb0940a0f342bc18bfcc81b0
AlejandroYanes <alejandro.yanes94@gmail.com>
2020-10-21 18:18:46 +0000

    Deploying to gh-pages from  @ b4e57acface031444895645821c29c2ddfc5d391 🚀

---
+++ precache-manifest.a00578e3415ef9720eda241e1d2f5184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb14d1acce98054dc81d3e982ddbaba7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0d2b8901fc9a67256d4",
+    "url": "/static/css/main.74fb3150.chunk.css"
+  },
+  {
+    "revision": "10ca29d9e26c086772a8",
+    "url": "/static/js/2.a7d89d01.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a7d89d01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0d2b8901fc9a67256d4",
+    "url": "/static/js/main.dc162ecc.chunk.js"
+  },
+  {
+    "revision": "1249128c0d9a3b9f5c67",
+    "url": "/static/js/runtime-main.6a3b67ea.js"
+  }
+]);
